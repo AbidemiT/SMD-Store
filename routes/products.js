ec144b0b1dc055a1114d70b63c21e8a99488066c
AbidemiT <abidemis01@gmail.com>
2020-02-08 09:03:32 +0100

    completed update and delete logic for products route

--- routes/products.js
+++ routes/products.js
@@ -12,13 +12,19 @@ const {
 // @desc Get All Products
 // access Private
 
-router.get("/",auth, async(req,res) => {
-    const products = await Product.find({user: req.user.id});
-    
+router.get("/", auth, async (req, res) => {
+    const products = await Product.find({
+        user: req.user.id
+    });
+
     try {
-        res.status(200).json({products});
+        res.status(200).json({
+            products
+        });
     } catch (err) {
-        res.status(500).json({err: "Server Error"});
+        res.status(500).json({
+            err: "Server Error"
+        });
     }
 })
 
@@ -26,8 +32,15 @@ router.get("/",auth, async(req,res) => {
 // @desc Add new product
 // @access Public
 
-router.post("/",auth,[check("name","Enter Product Name").not().isEmpty(), check("brand", "Enter product brand").not().isEmpty(),check("price", "Enter product price").not().isEmpty().isNumeric(), check("quantity", "Enter No of products available").not().isEmpty()], async(req,res) => {
-    const {name, image, brand, price, quantity, description} = req.body;
+router.post("/", auth, [check("name", "Enter Product Name").not().isEmpty(), check("brand", "Enter product brand").not().isEmpty(), check("price", "Enter product price").not().isEmpty().isNumeric(), check("quantity", "Enter No of products available").not().isEmpty()], async (req, res) => {
+    const {
+        name,
+        image,
+        brand,
+        price,
+        quantity,
+        description
+    } = req.body;
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -50,7 +63,9 @@ router.post("/",auth,[check("name","Enter Product Name").not().isEmpty(), check(
         await newProduct.save();
         res.status(201).json(newProduct);
     } catch (err) {
-        res.status(500).json({err: "Server Error"});
+        res.status(500).json({
+            err: "Server Error"
+        });
     }
 })
 
@@ -58,16 +73,79 @@ router.post("/",auth,[check("name","Enter Product Name").not().isEmpty(), check(
 // @desc Update product
 // @access Private
 
-router.put("/:id", (req,res) => {
-    res.send("Update Product");
+router.put("/:id", auth, async (req, res) => {
+    const {
+        name,
+        image,
+        brand,
+        price,
+        quantity,
+        description
+    } = req.body;
+    const id = req.params.id
+
+    //create product field
+    const productFields = {};
+    if (name) productFields.name = name;
+    if (image) productFields.image = image;
+    if (brand) productFields.brand = brand;
+    if (price) productFields.price = price;
+    if (quantity) productFields.quantity = quantity;
+    if (description) productFields.description = description;
+
+    try {
+        let product = await Product.findById(id);
+        if (!product) res.status(404).json({
+            err: "Product not found"
+        });
+
+        if (product.user.toString() !== req.user.id) res.status(401).json({
+            err: "Unauthorized Action"
+        });
+
+        product = await Product.findByIdAndUpdate(id, {
+            $set: productFields
+        }, {
+            new: true
+        });
+
+        res.status(200).json({
+            status: "Update Successful",
+            product
+        })
+    } catch (error) {
+        res.status(500).json({
+            err: "Server Error"
+        });
+    }
 })
 
 // @method DELETE api/products/
 // @desc Delete product
 // @access Private
 
-router.delete("/:id", (req,res) => {
-    res.send("Delete Product");
+router.delete("/:id", auth, async(req, res) => {
+    const id = req.params.id;
+    try {
+        let product = await Product.findById(id);
+        if (!product) res.status(404).json({
+            err: "Product not found"
+        });
+
+        if (product.user.toString() !== req.user.id) res.status(401).json({
+            err: "Unauthorized Action"
+        });
+
+        await Product.findByIdAndRemove(id);
+
+        res.status(200).json({
+            status: "Delete Successful",
+        })
+    } catch (error) {
+        res.status(500).json({
+            err: "Server Error"
+        });
+    }
 })
 
 
